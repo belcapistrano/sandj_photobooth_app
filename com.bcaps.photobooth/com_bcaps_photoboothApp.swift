7cbe4f0253bdf5597b71f2664ed8ee1600caee29
Bebel Capistrano <bebelcapistrano@aol.com>
2025-10-26 22:48:01 -0400

    Initial Commit

---
+++ com.bcaps.photobooth/com_bcaps_photoboothApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_bcaps_photoboothApp.swift
+//  com.bcaps.photobooth
+//
+//  Created by Bebel Capistrano on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct com_bcaps_photoboothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
